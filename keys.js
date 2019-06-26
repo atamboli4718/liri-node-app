6dba8b83887e018fbc4678590cd76152cf3908e3
Alicia Tamboli <atamboli4718@gmail.com>
2019-06-26 08:24:12 -0400

    added all files and started api keys

---
+++ keys.js
@@ -0,0 +1,7 @@
+js
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,//fa126dc3a4b346d6ad12e563769bbf0e
+  secret: process.env.SPOTIFY_SECRET //8286cded382a47b4a132030e32231a0b
+};
