ab4614ae49f66fc2d39b07ae079f10a7d8ee8feb
Alicia Tamboli <atamboli4718@gmail.com>
2019-06-29 16:39:42 -0400

    got bands in town working and writing to the text file

--- liri.js
+++ liri.js
@@ -7,14 +7,13 @@ display information from api to the screen
 
 //packages
 require('dotenv').config();
-//const APIs = require('./keys');
 const inquirer = require('inquirer');
 const axios = require("axios");
-
-
+var http = require("http");
 
 //apikeys
 const omdbKey = process.env.OMDB_apikey;
+const bandsKey = process.env.BANDSinTOWN_app_id;
 console.log(omdbKey);
 
 const divider = "\n------------------------------------------------------------\n\n";
@@ -22,53 +21,119 @@ var fs = require("fs");
 
 //ask the user what they would like to research
 inquirer
-    .prompt([
-        {
+    .prompt([{
         type: 'list',
         message: 'What can I help you with today?',
-        choices: ['Song Request','Movie Information','Concert Information'],
+        choices: ['Song Request', 'Movie Information', 'Concert Information'],
         name: 'reqSel',
-        }    
-    ]).then(function(inquirerResponse){
+    }]).then(function (inquirerResponse) {
         console.log(inquirerResponse.reqSel);
-        switch(inquirerResponse.reqSel) {
+        switch (inquirerResponse.reqSel) {
             case 'Movie Information':
                 inquirer
-                    .prompt([
-                        {
+                    .prompt([{
                         type: 'input',
                         message: 'What movie are you interested in?',
                         name: 'movieSel',
-                        }
-                    ]).then(function(inquirerResponse){
-                        let movieSel=inquirerResponse.movieSel
+                    }]).then(function (inquirerResponse) {
+                        let movieSel = inquirerResponse.movieSel
                         movieFun(movieSel);
                     })
+                break;
+            case 'Song Request':
+                inquirer
+                    .prompt([{
+                        type: 'input',
+                        message: 'Which song would you like information on?',
+                        name: 'songSel',
+                    }]).then(function (inquirerResponse) {
+                        let songSel = inquirerResponse.songSel
+                        songFun(songSel);
+                    })
+                break;
+            case 'Concert Information':
+                inquirer
+                    .prompt([{
+                        type: 'input',
+                        message: 'What concert are you interested in?',
+                        name: 'concertSel',
+                    }]).then(function (inquirerResponse) {
+                        let concertSel = inquirerResponse.concertSel
+                        concertFun(concertSel);
+                    });
+
         }
     })
 
 
 //function for searching omdb
 function movieFun(movieSel) {
-    axios.get('http://www.omdbapi.com/?t='+movieSel+'&apikey='+omdbKey).then(
-        function(response) {
-            let omdbResponse=response.data;
+    axios.get('http://www.omdbapi.com/?t=' + movieSel + '&apikey=' + omdbKey).then(
+        function (response) {
+            let omdbResponse = response.data;
             let movieData = [
-                'title: '+ omdbResponse.Title,
-                'year: '+ omdbResponse.Year,
-                'imdb rating: '+omdbResponse.imdbRating,
+                'title: ' + omdbResponse.Title,
+                'year: ' + omdbResponse.Year,
+                'imdb rating: ' + omdbResponse.imdbRating,
                 //'rotten tomatoes rating: '+omdbResponse.
-                'country: '+omdbResponse.Country,
-                'language: '+omdbResponse.Language,
-                'plot: '+omdbResponse.Plot,
-                'actors: '+omdbResponse.Actors,
+                'country: ' + omdbResponse.Country,
+                'language: ' + omdbResponse.Language,
+                'plot: ' + omdbResponse.Plot,
+                'actors: ' + omdbResponse.Actors,
             ];
             console.log(movieData);
-            fs.appendFile('log.txt',movieSel+movieData+divider,function(err){
+            fs.appendFileSync('log.txt', movieData + '\n', function (err) {
                 if (err) throw err;
-                console.log('Error adding to log.txt file');
-              });
-    }).catch(function(error){
+                console.log('Error adding to log.txt file' + err);
+            });
+        }).catch(function (error) {
         console.log("Error", error.message);
     })
+}
+
+function songFun(songSel) {
+    axios.get('http://www.omdbapi.com/?t=' + movieSel + '&apikey=' + omdbKey).then(
+        function (response) {
+            let omdbResponse = response.data;
+            let songData = [
+                'title: ' + omdbResponse.Title,
+                'year: ' + omdbResponse.Year,
+                'imdb rating: ' + omdbResponse.imdbRating,
+                //'rotten tomatoes rating: '+omdbResponse.
+                'country: ' + omdbResponse.Country,
+                'language: ' + omdbResponse.Language,
+                'plot: ' + omdbResponse.Plot,
+                'actors: ' + omdbResponse.Actors,
+            ];
+            console.log(songData);
+            fs.appendFileSync('log.txt', songData + '\n', function (err) {
+                if (err) throw err;
+                console.log('Error adding to log.txt file' + err);
+            });
+        }).catch(function (error) {
+        console.log("Error", error.message);
+    })
+
+}
+
+function concertFun(concertSel) {
+    axios.get("https://rest.bandsintown.com/artists/" + concertSel + "/events?app_id=" + bandsKey).then(
+        function (response) {
+            let bandsResponse = response.data;
+            for (var i = 0; i < bandsResponse.length; i++) {
+                let concertData = [
+                    'name of venue: ' + bandsResponse[i].venue.name,
+                    'venue location: ' + bandsResponse[i].venue.city,
+                    'date: ' + bandsResponse[i].datetime
+                ];
+                console.log(concertData);
+                fs.appendFileSync('log.txt', concertData + '\n', function (err) {
+                    if (err) throw err;
+                    console.log('Error adding to log.txt file' + err);
+                });
+            }
+        }).catch(function (error) {
+        console.log("Error", error.message);
+    })
+
 }
