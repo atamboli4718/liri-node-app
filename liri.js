e7dd93a680f964b564d55af80a151c738673715b
Alicia Tamboli <atamboli4718@gmail.com>
2019-06-28 21:50:10 -0400

    omdb api is working and logging to the console getting an error adding to the txt file

--- liri.js
+++ liri.js
@@ -6,14 +6,19 @@ display information from api to the screen
 */
 
 //packages
-const APIs = require('./keys');
+require('dotenv').config();
+//const APIs = require('./keys');
 const inquirer = require('inquirer');
 const axios = require("axios");
 
 
-//apikeys
-const omdbKey = keys.omdb.id
 
+//apikeys
+const omdbKey = process.env.OMDB_apikey;
+console.log(omdbKey);
+
+const divider = "\n------------------------------------------------------------\n\n";
+var fs = require("fs");
 
 //ask the user what they would like to research
 inquirer
@@ -28,7 +33,6 @@ inquirer
         console.log(inquirerResponse.reqSel);
         switch(inquirerResponse.reqSel) {
             case 'Movie Information':
-                console.log("movieStatement");
                 inquirer
                     .prompt([
                         {
@@ -40,15 +44,31 @@ inquirer
                         let movieSel=inquirerResponse.movieSel
                         movieFun(movieSel);
                     })
-
         }
     })
 
 
 //function for searching omdb
 function movieFun(movieSel) {
-    axios.get('http://www.omdbapi.com/?t='+movieSel+'"&apikey='+omdbKey).then(
-        function (response) {
-            console.log(response);
+    axios.get('http://www.omdbapi.com/?t='+movieSel+'&apikey='+omdbKey).then(
+        function(response) {
+            let omdbResponse=response.data;
+            let movieData = [
+                'title: '+ omdbResponse.Title,
+                'year: '+ omdbResponse.Year,
+                'imdb rating: '+omdbResponse.imdbRating,
+                //'rotten tomatoes rating: '+omdbResponse.
+                'country: '+omdbResponse.Country,
+                'language: '+omdbResponse.Language,
+                'plot: '+omdbResponse.Plot,
+                'actors: '+omdbResponse.Actors,
+            ];
+            console.log(movieData);
+            fs.appendFile('log.txt',movieSel+movieData+divider,function(err){
+                if (err) throw err;
+                console.log('Error adding to log.txt file');
+              });
+    }).catch(function(error){
+        console.log("Error", error.message);
     })
 }
